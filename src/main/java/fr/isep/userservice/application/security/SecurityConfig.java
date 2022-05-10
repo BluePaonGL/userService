7cfd47a10fa8b1adfd2d4ad7c141e15f2c1902d2
Artcann <art.cann@orange.fr>
2022-05-10 18:36:58 +0200

    CORS handling

---
+++ src/main/java/fr/isep/userservice/application/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package fr.isep.userservice.application.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public WebMvcConfigurer corsConfiguration() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods(HttpMethod.GET.toString(), HttpMethod.POST.toString(),
+                                HttpMethod.PUT.toString(), HttpMethod.DELETE.toString(), HttpMethod.OPTIONS.toString())
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
