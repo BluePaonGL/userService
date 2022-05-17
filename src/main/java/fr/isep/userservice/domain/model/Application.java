7f56cf9cfc79e7cd5e6f551c49e98bee92b15f56
Artcann <art.cann@orange.fr>
2022-05-17 22:58:55 +0200

    Add Unit Test

--- src/main/java/fr/isep/userservice/domain/model/Application.java
+++ src/main/java/fr/isep/userservice/domain/model/Application.java
@@ -2,12 +2,14 @@ package fr.isep.userservice.domain.model;
 
 import fr.isep.userservice.domain.model.enums.ApplicationStatusEnum;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Application {
     private String applicationId;
     private ApplicationStatusEnum status;
