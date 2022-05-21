28fc948c514bad3f7e06d7e75281fa69cfbbc966
Artcann <art.cann@orange.fr>
2022-05-21 22:34:51 +0200

    Change page method to POST

--- src/main/java/fr/isep/userservice/application/controller/ApplicationController.java
+++ src/main/java/fr/isep/userservice/application/controller/ApplicationController.java
@@ -24,7 +24,7 @@ public class ApplicationController {
 
     private final ApplicationServicePort applicationServicePort;
 
-    @GetMapping()
+    @PostMapping()
     public ResponseEntity<Page<Application>> pageApplicationByUser(
             @RequestParam(required = false) ApplicationStatusEnum status,
             @RequestParam(required = false) String userId,
